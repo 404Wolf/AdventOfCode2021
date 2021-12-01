86243a521c373d64b8bdd3b583ddb12c3573dd0c
Wolf <Wolf@techy.cc>
2021-12-01 01:06:49 -0500

    Added comments
    Removed unused import (json)

--- day1/part2.py
+++ day1/part2.py
@@ -1,4 +1,3 @@
-import json
 from math import inf 
 
 #load input file
@@ -13,23 +12,27 @@ while True:
     currentWindowCounter = 0
     
     for depth in depths:
-        windows.append(depths[currentWindowCounter:currentWindowCounter+3])
+        windows.append(depths[currentWindowCounter:currentWindowCounter+3]) #windows consist of three entries
+        #continue moving forward in depths until the window consists of three entires (a full window is three entries)
         currentWindowCounter += 4
         if len(windows[-1]) == 3:
-            break
+            break #break the loop and remove the first depth
 
     del depths[0]
+    # if there are less than three depths left, a full window is not able to be created
     if len(depths) < 3:
         break
 
-#calculate number of changes that are increased
+#calculate for each change vs it's respective previous
+#if it's a positive change (True) or is the same/is less (False)
 changes = []
 previous = inf
-for window in map(sum, windows):
+for window in map(sum, windows): #the sum of each window
     if window > previous:
         changes.append(True)
     elif window <= previous:
         changes.append(False)
     previous = window
 
+#output the number of positive changes
 print(sum(changes))
