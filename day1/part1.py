bbf6557999d02ff1d3f28215b487425e22727cba
Wolf <Wolf@techy.cc>
2021-12-01 01:06:59 -0500

    Added comments

--- day1/part1.py
+++ day1/part1.py
@@ -1,10 +1,13 @@
 from math import inf
 
+#load input file
 with open("input.txt") as input:
     depths = input.read().split("\n")
     depths = map(int, depths)
     depths = tuple(depths)
 
+#calculate for each change vs it's respective previous
+#if it's a positive change (True) or is the same/is less (False)
 changes = []
 previous = inf
 for depth in depths:
@@ -14,4 +17,5 @@ for depth in depths:
         changes.append(False)
     previous = depth
 
+#output the number of positive changes
 print(sum(changes))
