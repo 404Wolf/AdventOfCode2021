3a555ce3653b27bae4cf65ba8bc28d9412092b22
Twilak <64438910+hair@users.noreply.github.com>
2021-12-05 07:19:45 +0000

    began work on day 3 part 2

--- day1/part2.py
+++ day1/part2.py
@@ -1,3 +1,4 @@
+
 from math import inf
 
 # load input file
--- day3/part2.py
+++ day3/part2.py
@@ -1,10 +1,38 @@
+# load in input.txt as a newline seperated array
 entries = open("input.txt").read().split("\n")
 
-# find oxygen generator rating
-for entry in entries:
-    mostCommon = 1
-    if entry.count("1") < entry.count("0"):
-        mostCommon = 0
-    print(entry,mostCommon)
+# the equivalent of mapping the following function onto each value in entries:
+# def rowToList(row):
+#     row = list(row) / eg: "0101" -> ("0", "1", "0", "1")
+#     row = map(int, row) / eg: ("0", "1", "0", "1") -> (0, 1, 0, 1)
+#     return row
+entries = map(lambda entry: list(map(int, list(entry))), entries)
 
-# find co2 scrubber rating
+# make an itterable of each coloumn of the multidimentional itterable
+columns = zip(*entries)  # unpack entries into zip() function
+
+def mostCommon(itterable):
+  """Returns most common element in itterable"""
+  if itterable.count("0") > itterable.count("1"):
+    return 0
+  else:
+    return 1
+    
+goodEntries = []
+
+while len(goodEntries) != 1:
+  for entry in entries:
+    for bitIndex, bit in enumerate(entry):
+      # We are getting the value of the column, but using the index of the entry
+      success = True
+      if mostCommon(columns[bitIndex]) != bit:
+        success = False
+
+    if success:
+      goodEntries.append(entry)
+      
+  entries = goodEntries
+  
+print(goodEntries)
+# # newBit = int(newBit, 2)
+# # print(newBit)
